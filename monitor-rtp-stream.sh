40af0693d0e2a7c2b86b0ccbd9f7ba0066dbf121
jmillay <blindluck9@gmail.com>
2015-02-10 13:35:59 -0500

    Updated file paths

--- monitor-rtp-stream.sh
+++ monitor-rtp-stream.sh
@@ -7,8 +7,8 @@ fi
 
 c=1
 while true; do 
-	`/usr/local/bin/rtpdump -F short -t 1 -o /root/$1-$c.rtp $1/7534`;  
-	/usr/bin/python /root/parse-rtp.py /root/$1-$c.rtp >> /root/rtplog.log; 
+	`/usr/local/bin/rtpdump -F short -t 1 -o /root/iptv-rtp-probe/$1-$c.rtp $1/7534`;  
+	/usr/bin/python /root/iptv-rtp-probe/parse-rtp.py /root/iptv-rtp-probe/$1-$c.rtp >> /root/iptv-rtp-probe/rtplog.log; 
 	/bin/rm /root/$1-$c.rtp;
 	(( c++ ))
 done
--- monitor-start.sh
+++ monitor-start.sh
@@ -3,6 +3,6 @@
 /usr/sbin/smcroute -j eth0 239.192.37.113
 /usr/sbin/smcroute -j eth0 239.192.37.44
 /usr/sbin/smcroute -j eth0 239.192.37.30
-/root/monitor-rtp-stream.sh 239.192.37.113 &
-/root/monitor-rtp-stream.sh 239.192.37.44 &
-/root/monitor-rtp-stream.sh 239.192.37.30 &
+/root/iptv-rtp-probe/monitor-rtp-stream.sh 239.192.37.113 &
+/root/iptv-rtp-probe/monitor-rtp-stream.sh 239.192.37.44 &
+/root/iptv-rtp-probe/monitor-rtp-stream.sh 239.192.37.30 &
