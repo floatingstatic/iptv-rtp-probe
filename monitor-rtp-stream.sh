e5eed1a32976458f2eaaa68458f0db5876c970ce
jmillay <blindluck9@gmail.com>
2015-02-10 14:17:18 -0500

    Fixed path in monitor-rtp-stream.sh

--- monitor-rtp-stream.sh
+++ monitor-rtp-stream.sh
@@ -9,7 +9,7 @@ c=1
 while true; do 
 	`/usr/local/bin/rtpdump -F short -t 1 -o /root/iptv-rtp-probe/$1-$c.rtp $1/7534`;  
 	/usr/bin/python /root/iptv-rtp-probe/parse-rtp.py /root/iptv-rtp-probe/$1-$c.rtp >> /root/iptv-rtp-probe/rtplog.log; 
-	/bin/rm /root/$1-$c.rtp;
+	/bin/rm /root/iptv-rtp-probe/$1-$c.rtp;
 	(( c++ ))
 done
 
