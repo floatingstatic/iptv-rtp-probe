59bf407354944bd030834d4ee4befeef8940d545
jmillay <blindluck9@gmail.com>
2015-03-08 11:26:10 -0400

    Updated paths in parse-rtp.py and monitor-rtp-stream.sh

--- monitor-rtp-stream.sh
+++ monitor-rtp-stream.sh
@@ -6,6 +6,7 @@ if [ -z "$1" ]; then
 fi
 
 c=1
+`cd /root/iptv-rtp-probe`;
 while true; do 
 	`/usr/local/bin/rtpdump -F short -t 1 -o /root/iptv-rtp-probe/$1-$c.rtp $1/7534`;  
 	/usr/bin/python /root/iptv-rtp-probe/parse-rtp.py /root/iptv-rtp-probe/$1-$c.rtp >> /root/iptv-rtp-probe/rtplog.log; 
